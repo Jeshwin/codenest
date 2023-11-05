7d0036619ac5397fae08f49d35c94e1e59654e02
Jeshwin Prince <jeshwinjprince@gmail.com>
2023-11-05 13:43:41 -0800

    add tabs to editor component

--- components/editor/editor.jsx
+++ components/editor/editor.jsx
@@ -1,40 +1,16 @@
 "use client"
 
-import { useEffect } from "react"
-
-const tabLength = 4
+import { useEffect, useRef } from "react"
+import { updateLineNumbers, readKey } from "./editorfunctions"
+import { TabData } from "./testdata"
 
 export default function Editor() {
+    const currentTab = useRef(0)
+    const tabs = useRef(TabData)
+
     useEffect(() => {
         document.getElementById("mytextarea").value =
             "This\nis\nthe\nstarting\n    info"
-
-        function lineNumbers(numLines) {
-            var text = ""
-            for (var i = 1; i <= numLines; i++) {
-                text += i + "\n"
-            }
-            document.getElementById("line-numbers").value = text
-        }
-
-        function updateLineNumbers() {
-            var text = document.getElementById("mytextarea").value
-            var lines = text.split(/\r|\r\n|\n/)
-            var count = lines.length
-            if (count >= 1000) {
-                document.getElementById("line-numbers").style.width = "6ch"
-            }
-            if (count >= 10000) {
-                document.getElementById("line-numbers").style.width = "7ch"
-            }
-            if (count >= 100000) {
-                document.getElementById("line-numbers").style.width = "8ch"
-            }
-            if (count >= 1000000) {
-                document.getElementById("line-numbers").style.width = "9ch"
-            }
-            lineNumbers(count)
-        }
 
         document
             .getElementById("mytextarea")
@@ -42,119 +18,9 @@ export default function Editor() {
         document
             .getElementById("mytextarea")
             .addEventListener("paste", updateLineNumbers)
-
-        function readKey(e) {
-            var el = document.getElementById("mytextarea")
-
-            if (
-                e.key == "'" ||
-                e.key == '"' ||
-                e.key == "{" ||
-                e.key == "(" ||
-                e.key == "[" ||
-                e.key == "Tab" ||
-                e.key == "Enter"
-            ) {
-                e.preventDefault()
-                var start = el.selectionStart
-                var end = el.selectionEnd
-            }
-
-            if (e.key == "'") {
-                el.value =
-                    el.value.substring(0, start) +
-                    "''" +
-                    el.value.substring(end)
-                el.selectionStart = start + 1
-                el.selectionEnd = start + 1
-            } else if (e.key == '"') {
-                el.value =
-                    el.value.substring(0, start) +
-                    '""' +
-                    el.value.substring(end)
-                el.selectionStart = start + 1
-                el.selectionEnd = start + 1
-            } else if (e.key == "(") {
-                el.value =
-                    el.value.substring(0, start) +
-                    "()" +
-                    el.value.substring(end)
-                el.selectionStart = start + 1
-                el.selectionEnd = start + 1
-            } else if (e.key == "[") {
-                el.value =
-                    el.value.substring(0, start) +
-                    "[]" +
-                    el.value.substring(end)
-                el.selectionStart = start + 1
-                el.selectionEnd = start + 1
-            } else if (e.key == "{") {
-                el.value =
-                    el.value.substring(0, start) +
-                    "{}" +
-                    el.value.substring(end)
-                el.selectionStart = start + 1
-                el.selectionEnd = start + 1
-            } else if (e.key == "Tab") {
-                var spaces = ""
-                for (var i = 0; i < tabLength; i++) {
-                    spaces += " "
-                }
-                el.value =
-                    el.value.substring(0, start) +
-                    spaces +
-                    el.value.substring(end)
-                el.selectionStart = start + tabLength
-                el.selectionEnd = start + tabLength
-            } else if (e.key === "Enter") {
-                var line = el.value.substring(0, end)
-                var lastLine = line.substring(line.lastIndexOf("\n") + 1)
-                var numSpaces = 0
-                while (lastLine.charAt(0) == " ") {
-                    lastLine = lastLine.substring(1)
-                    numSpaces++
-                }
-                var spaces = ""
-                for (var i = 0; i < numSpaces; i++) {
-                    spaces += " "
-                }
-                if (
-                    el.value.charAt(el.selectionStart - 1) == "{" &&
-                    el.value.charAt(el.selectionStart) == "}"
-                ) {
-                    var spaces2 = ""
-                    for (var i = 0; i < tabLength; i++) {
-                        spaces2 += " "
-                    }
-                    spaces += spaces2 + "\n" + spaces
-                    numSpaces += tabLength
-                }
-                el.value =
-                    el.value.substring(0, start) +
-                    "\n" +
-                    spaces +
-                    el.value.substring(end)
-                el.selectionStart = start + numSpaces + 1
-                el.selectionEnd = start + numSpaces + 1
-                var count = el.value.split(/\r|\r\n|\n/).length
-                lineNumbers(count)
-                el.blur()
-                el.focus()
-            }
-
-            updateLineNumbers()
-        }
-
         document
             .getElementById("mytextarea")
             .addEventListener("keydown", (e) => readKey(e))
-
-        document.getElementById("mytextarea").onscroll = function () {
-            document
-                .getElementById("line-numbers")
-                .scrollTo(0, document.getElementById("mytextarea").scrollTop)
-            updateLineNumbers()
-        }
 
         updateLineNumbers()
 
@@ -171,22 +37,94 @@ export default function Editor() {
         }
     }, [])
 
+    useEffect(() => {
+        const editorTextArea = document.getElementById("mytextarea")
+
+        for (var i = 0; i < tabs.current.length; i++) {
+            var tab = document.createElement("div")
+
+            tab.innerHTML = `<span>Tab1.css</span><span class="close-tab block w-5 h-5 ml-2 p-0 rounded-md text-center hover:bg-[var(--bg-error)]">&#66338;</span>`
+            tab.setAttribute("name", i)
+            tab.firstChild.setAttribute("name", i)
+            tab.children[1].setAttribute("name", i)
+            tab.className =
+                "tab flex h-min py-2 px-5 hover:brightness-150 bg-[var(--light-bg-2)] dark:bg-[var(--dark-bg-2)] text-[var(--light-fg-1)] dark:text-[var(--dark-fg-1)] cursor-pointer"
+            tab.firstChild.innerHTML = tabs.current[i].filename
+
+            document.getElementById("menubar").appendChild(tab)
+
+            tab.addEventListener("click", function (e) {
+                if (currentTab.current == e.target.getAttribute("name")) return
+                tabs.current[currentTab.current].cursorStart =
+                    editorTextArea.selectionStart
+                tabs.current[currentTab.current].cursorEnd =
+                    editorTextArea.selectionEnd
+                tabs.current[currentTab.current].content = editorTextArea.value
+
+                editorTextArea.value =
+                    tabs.current[e.target.getAttribute("name")].content
+                editorTextArea.blur()
+                editorTextArea.focus()
+                editorTextArea.selectionStart =
+                    tabs.current[e.target.getAttribute("name")].cursorStart
+                editorTextArea.selectionEnd =
+                    tabs.current[e.target.getAttribute("name")].cursorEnd
+                currentTab.current = e.target.getAttribute("name")
+                var allTabs = document.getElementsByClassName("tab")
+                for (var i = 0; i < allTabs.length; i++) {
+                    // allTabs[i].classList.remove("selected-tab")
+                    allTabs[i].classList.remove("brightness-150")
+                }
+                if (e.target.parentNode.className.includes("tab")) {
+                    // e.target.parentNode.classList.add("selected-tab")
+                    e.target.parentNode.classList.add("brightness-150")
+                } else {
+                    // e.target.classList.add("selected-tab")
+                    e.target.classList.add("brightness-150")
+                }
+            })
+
+            tab.children[1].addEventListener("click", function (e) {
+                e.target.parentNode.firstChild.value
+                e.target.parentNode.remove()
+            })
+        }
+        document
+            .getElementById("menubar")
+            .firstChild.classList.add("brightness-150")
+        editorTextArea.value = tabs.current[currentTab.current].content
+    }, [])
+
+    function handleEditorScroll() {
+        document
+            .getElementById("line-numbers")
+            .scrollTo(0, document.getElementById("mytextarea").scrollTop)
+        updateLineNumbers()
+    }
+
     return (
-        <div id="wrapper" className="w-full flex font-mono">
-            <textarea
-                id="line-numbers"
-                className=" min-w-[5ch] max-w-[9ch] text-right flex-none pr-[1ch] border-none outline-none rounded-l-lg overflow-hidden resize-none bg-[var(--light-bg-2)] text-[var(--light-fg-1)] dark:bg-[var(--dark-bg-2)] dark:text-[var(--dark-fg-1)]"
-                cols="1"
-                rows="10"
-                readOnly
-            ></textarea>
-            <textarea
-                id="mytextarea"
-                autoComplete="off"
-                autoCorrect="off"
-                spellCheck="false"
-                className="w-full pl-2 border-none outline-none whitespace-pre rounded-r-lg overflow-wrap-normal overflow-x-scroll  resize-none bg-[var(--light-bg-1)] text-[var(--light-fg-1)] dark:bg-[var(--dark-bg-1)] dark:text-[var(--dark-fg-1)]"
-            ></textarea>
-        </div>
+        <>
+            <div
+                id="menubar"
+                className="h-9 flex font-mono text-sm bg-[var(--light-bg-2)] dark:bg-[var(--dark-bg-2)] overflow-x-scroll"
+            ></div>
+            <div id="wrapper" className="w-full h-full flex font-mono">
+                <textarea
+                    id="line-numbers"
+                    className=" min-w-[5ch] max-w-[9ch] text-right flex-none pr-[1ch] border-none outline-none rounded-bl-lg overflow-hidden resize-none bg-[var(--light-bg-2)] text-[var(--light-fg-1)] dark:bg-[var(--dark-bg-2)] dark:text-[var(--dark-fg-1)]"
+                    cols="1"
+                    rows="10"
+                    readOnly
+                ></textarea>
+                <textarea
+                    id="mytextarea"
+                    onScroll={handleEditorScroll}
+                    autoComplete="off"
+                    autoCorrect="off"
+                    spellCheck="false"
+                    className="w-full pl-2 border-none outline-none whitespace-pre rounded-br-lg overflow-wrap-normal overflow-x-scroll resize-none bg-[var(--light-bg-1)] text-[var(--light-fg-1)] dark:bg-[var(--dark-bg-1)] dark:text-[var(--dark-fg-1)]"
+                ></textarea>
+            </div>
+        </>
     )
 }
