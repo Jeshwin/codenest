9896d21b9d0d4ce1c3ec33cb1e17911fdcc722bb
Jeshwin <jeshwinjprince@gmail.com>
2023-12-05 14:40:56 -0800

    add auth0 integration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "s.gravatar.com",
+                port: "",
+                pathname: "/avatar/**",
+            },
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+                port: "",
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "avatars.githubusercontent.com",
+                port: "",
+                pathname: "/**",
+            },
+        ],
+    },
 }
 
 module.exports = nextConfig
