0f074175b850781939a0b8e3974f45dc8726e597
Jeshwin Prince <jeshwinjprince@gmail.com>
2024-11-21 11:52:07 -0800

    connect codemirror to a socket.io back end

--- src/components/codespace/codemirror/mirror.tsx
+++ src/components/codespace/codemirror/mirror.tsx
@@ -4,17 +4,29 @@ import {useState, useCallback, useEffect} from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import {loadLanguage} from "@uiw/codemirror-extensions-langs";
 import {myDarkTheme, myLightTheme} from "./themes";
+import {io, Socket} from "socket.io-client";
 
 export default function CodeEditor() {
     const [value, setValue] = useState("console.log('hello world!');");
     const [theme, setTheme] = useState("light");
+    const [socket, setSocket] = useState<Socket>();
+
+    useEffect(() => {
+        // Initialize socket connection
+        const socketInstance = io("http://localhost:3333");
+        setSocket(socketInstance);
+
+        // Clean up socket connection on component unmount
+        return () => {
+            socketInstance.disconnect();
+        };
+    }, []);
 
     useEffect(() => {
         // Listen to changes in the `data-theme` attribute
         const observer = new MutationObserver(() => {
-            const currentTheme = document.documentElement.getAttribute(
-                "data-theme"
-            );
+            const currentTheme =
+                document.documentElement.getAttribute("data-theme");
             setTheme(currentTheme);
         });
 
@@ -26,10 +38,19 @@ export default function CodeEditor() {
         return () => observer.disconnect(); // Cleanup observer on unmount
     }, []);
 
-    const onChange = useCallback((val, _viewUpdate) => {
-        console.log("val:", val);
-        setValue(val);
-    }, []);
+    const onChange = useCallback(
+        (val, viewUpdate) => {
+            console.log("val:", val);
+            console.log("viewUpdate:", viewUpdate);
+            setValue(val);
+
+            // Send updated content to the server
+            if (socket) {
+                socket.emit("message", val);
+            }
+        },
+        [socket]
+    );
 
     return (
         <CodeMirror
