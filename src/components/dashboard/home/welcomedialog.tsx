14120fd653b2ef50e78570005a63e524ea872cb6
Jeshwin Prince <jeshwinjprince@gmail.com>
2025-01-15 10:18:59 -0800

    <backend> Add user info back end using AWS Amplify data and storage

--- src/components/dashboard/home/welcomedialog.tsx
+++ src/components/dashboard/home/welcomedialog.tsx
@@ -29,6 +29,13 @@ import {
     FormLabel,
     FormMessage,
 } from "@/components/ui/form";
+import {uploadData} from "aws-amplify/storage";
+import {generateClient} from "aws-amplify/data";
+import {type Schema} from "@/../amplify/data/resource";
+
+const client = generateClient<Schema>({
+    authMode: "userPool",
+});
 
 const userFormSchema = z.object({
     username: z
@@ -50,10 +57,23 @@ const userFormSchema = z.object({
     bio: z.string().max(200, "Bio must not exceed 200 characters").optional(),
 });
 
+// Convert image types into extensions
+const mimeToExtension = {
+    "image/png": "png",
+    "image/jpeg": "jpg",
+    "image/jpg": "jpg",
+    "image/gif": "gif",
+    "image/webp": "webp",
+    "image/svg+xml": "svg",
+    "image/avif": "avif",
+    "image/tiff": "tiff",
+};
+
 export default function WelcomeDialog() {
     const [showDialog, setShowDialog] = useState(false);
     const [currentUser, setCurrentUser] = useState<AuthUser>();
-    const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
+    const [avatarImage, setAvatarImage] = useState<string | null>(null);
+    const [avatarImageType, setAvatarImageType] = useState<string | null>(null);
     const [defaultUsername, setDefaultUsername] = useState<string>();
 
     useEffect(() => {
@@ -69,21 +89,48 @@ export default function WelcomeDialog() {
         defaultValues: {
             username: "",
             firstName: "",
-            lastName: "",
-            bio: "",
         },
     });
 
-    function onSubmit(values: z.infer<typeof userFormSchema>) {
+    async function onSubmit(values: z.infer<typeof userFormSchema>) {
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
         console.log(values);
         console.log(
-            avatarUrl ||
+            avatarImage ||
                 `https://api.toucanny.net/avatar?userid=${
                     currentUser?.userId
                 }&w=${256}`
         );
+
+        // Get URL to profile photo
+        let profilePhotoURL;
+        if (avatarImage) {
+            // Upload avatar to storage
+            const result = await uploadData({
+                path: ({identityId}) =>
+                    `profile-pictures/${identityId}/pfp.${avatarImageType}`,
+                data: avatarImage,
+            }).result;
+            profilePhotoURL = result.path;
+        } else {
+            profilePhotoURL = `https://api.toucanny.net/avatar?userid=${currentUser?.userId}`;
+        }
+
+        // Create user info item
+        const {errors, data: newUserInfo} = await client.models.UserInfo.create(
+            {
+                username: values.username,
+                firstName: values.firstName,
+                lastName: values.lastName ?? "",
+                bio: values.bio ?? "",
+                profilePhoto: profilePhotoURL,
+            }
+        );
+        console.log(newUserInfo);
+        if (errors) {
+            console.error(errors);
+        }
     }
 
     useEffect(() => {
@@ -107,14 +154,17 @@ export default function WelcomeDialog() {
 
             const reader = new FileReader();
             reader.onload = () => {
-                setAvatarUrl(reader.result as string); // Set the uploaded image as the new avatar
+                setAvatarImage(reader.result as string); // Set the uploaded image as the new avatar
             };
             reader.readAsDataURL(file);
+            setAvatarImageType(mimeToExtension[file.type] || "unknown");
         }
     };
 
     const handleRemoveImage = () => {
-        setAvatarUrl(null); // Reset to default avatar
+        // Reset to default avatar
+        setAvatarImage(null);
+        setAvatarImageType(null);
     };
 
     useEffect(() => {
@@ -134,7 +184,7 @@ export default function WelcomeDialog() {
         <AlertDialog open={showDialog} onOpenChange={setShowDialog}>
             <Form {...form}>
                 <AlertDialogContent
-                    className="max-w-2xl"
+                    className="w-fit max-w-2xl"
                     onEscapeKeyDown={(e) => e.preventDefault()}
                 >
                     <AlertDialogHeader>
@@ -142,35 +192,36 @@ export default function WelcomeDialog() {
                             Welcome to CodeNest!
                         </AlertDialogTitle>
                     </AlertDialogHeader>
-                    <AlertDialogDescription className="flex gap-8">
-                        <div className="relative">
-                            <Avatar className="size-36 rounded-full mt-2">
+                    <AlertDialogDescription className="flex gap-4">
+                        <div className="relative size-36 w-fit overflow-hidden mt-2">
+                            <Avatar className="size-36 rounded-full">
                                 <AvatarImage
                                     src={
-                                        avatarUrl ||
+                                        avatarImage ||
                                         `https://api.toucanny.net/avatar?userid=${
                                             currentUser?.userId
                                         }&w=${256}`
                                     }
                                     alt={currentUser?.userId}
+                                    className="object-cover"
                                 />
                                 <AvatarFallback className="rounded-full">
                                     CN
                                 </AvatarFallback>
-                                <div className="cursor-pointer absolute bottom-0 left-0 right-0 bg-background/50 flex justify-center align-middle p-2 gap-1">
-                                    <label className="flex items-center gap-1 cursor-pointer">
-                                        <Camera className="size-4" />
-                                        Edit
-                                        <input
-                                            type="file"
-                                            accept="image/*"
-                                            className="hidden"
-                                            onChange={handleImageUpload}
-                                        />
-                                    </label>
-                                </div>
                             </Avatar>
-                            {avatarUrl && (
+                            <div className="cursor-pointer absolute bottom-0 left-0 right-0 bg-background/50 flex justify-center align-middle p-2 gap-1">
+                                <label className="flex items-center gap-1 cursor-pointer">
+                                    <Camera className="size-4" />
+                                    Edit
+                                    <input
+                                        type="file"
+                                        accept="image/*"
+                                        className="hidden"
+                                        onChange={handleImageUpload}
+                                    />
+                                </label>
+                            </div>
+                            {avatarImage && (
                                 <button
                                     onClick={handleRemoveImage}
                                     className="absolute top-0 right-0 bg-red-500 text-white p-1 rounded-full size-6"
@@ -181,7 +232,7 @@ export default function WelcomeDialog() {
                         </div>
                         <form
                             onSubmit={form.handleSubmit(onSubmit)}
-                            className="w-full flex flex-col gap-4"
+                            className="w-fit flex flex-col gap-4"
                         >
                             <FormField
                                 control={form.control}
