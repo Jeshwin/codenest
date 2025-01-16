a2e50b9d2cd3cb1ad01017d7370833f1f3e49e4b
Jeshwin Prince <jeshwinjprince@gmail.com>
2025-01-15 18:05:06 -0800

    Sync user info with other components in nav bars

--- src/components/codespace/navbar/userDropdown.tsx
+++ src/components/codespace/navbar/userDropdown.tsx
@@ -17,9 +17,19 @@ import {useRouter} from "next/navigation";
 import {useEffect, useState} from "react";
 import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
 import Link from "next/link";
+import {generateClient} from "aws-amplify/data";
+import {type Schema} from "@/../amplify/data/resource";
+import {getUrl} from "aws-amplify/storage";
+
+const client = generateClient<Schema>({
+    authMode: "userPool",
+});
 
 export default function UserDropdown() {
     const [currentUser, setCurrentUser] = useState<AuthUser>();
+    const [currentUserInfo, setCurrentUserInfo] =
+        useState<Schema["UserInfo"]["type"]>();
+    const [avatarURL, setAvatarURL] = useState<string>();
 
     useEffect(() => {
         const getData = async () => {
@@ -29,6 +39,52 @@ export default function UserDropdown() {
         getData();
     }, []);
 
+    useEffect(() => {
+        if (!currentUser) return;
+        // get a specific item
+        async function getUserInfo() {
+            const {data: userInfo, errors} = await client.models.UserInfo.list({
+                filter: {
+                    owner: {
+                        contains: currentUser.userId,
+                    },
+                },
+            });
+            userInfo.forEach((info) => {
+                console.log(info.owner);
+                console.log(info.owner == currentUser.userId);
+            });
+            console.dir({data: userInfo, errors});
+            setCurrentUserInfo(userInfo[0]);
+        }
+        console.log(currentUser.userId);
+        getUserInfo();
+    }, [currentUser]);
+
+    useEffect(() => {
+        if (!currentUserInfo) return;
+        const profilePhotoPieces = currentUserInfo.profilePhoto.split("/");
+        if (profilePhotoPieces[0].includes("http")) {
+            setAvatarURL(
+                `https://api.toucanny.net/avatar?userid=${
+                    currentUser?.userId
+                }&w=${256}`
+            );
+        } else {
+            async function generateURL() {
+                const generatedURL = await getUrl({
+                    path: currentUserInfo.profilePhoto,
+                    options: {
+                        expiresIn: 86400,
+                    },
+                });
+                console.log(generatedURL.url.toString());
+                setAvatarURL(generatedURL.url.toString());
+            }
+            generateURL();
+        }
+    }, [currentUser?.userId, currentUserInfo]);
+
     const router = useRouter();
     const handleSignOut = async () => {
         await signOut();
@@ -37,15 +93,14 @@ export default function UserDropdown() {
     };
 
     return (
-        currentUser && (
+        currentUser &&
+        currentUserInfo && (
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                     <Button variant="ghost" size="sm" className="size-10 p-2">
                         <Avatar className="size-6 rounded-full">
                             <AvatarImage
-                                src={`https://api.toucanny.net/avatar?userid=${
-                                    currentUser?.userId
-                                }&w=${256}`}
+                                src={avatarURL}
                                 alt={currentUser?.userId}
                             />
                             <AvatarFallback className="rounded-lg">
@@ -59,9 +114,7 @@ export default function UserDropdown() {
                         <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                             <Avatar className="size-8 rounded-full">
                                 <AvatarImage
-                                    src={`https://api.toucanny.net/avatar?userid=${
-                                        currentUser?.userId
-                                    }&w=${256}`}
+                                    src={avatarURL}
                                     alt={currentUser?.userId}
                                 />
                                 <AvatarFallback className="rounded-lg">
@@ -70,10 +123,11 @@ export default function UserDropdown() {
                             </Avatar>
                             <div className="grid flex-1 text-left text-sm leading-tight">
                                 <span className="truncate font-semibold">
-                                    {currentUser?.userId}
+                                    {currentUserInfo.firstName}{" "}
+                                    {currentUserInfo.lastName}
                                 </span>
                                 <span className="truncate text-xs">
-                                    {currentUser?.signInDetails?.loginId}
+                                    @{currentUserInfo.username}
                                 </span>
                             </div>
                         </div>
--- src/components/dashboard/nav-user.tsx
+++ src/components/dashboard/nav-user.tsx
@@ -23,10 +23,20 @@ import {signOut} from "aws-amplify/auth";
 import {useRouter} from "next/navigation";
 import {useEffect, useState} from "react";
 import Link from "next/link";
+import {generateClient} from "aws-amplify/data";
+import {type Schema} from "@/../amplify/data/resource";
+import {getUrl} from "aws-amplify/storage";
+
+const client = generateClient<Schema>({
+    authMode: "userPool",
+});
 
 export default function NavUser() {
     const {isMobile} = useSidebar();
     const [currentUser, setCurrentUser] = useState<AuthUser>();
+    const [currentUserInfo, setCurrentUserInfo] =
+        useState<Schema["UserInfo"]["type"]>();
+    const [avatarURL, setAvatarURL] = useState<string>();
 
     useEffect(() => {
         const getData = async () => {
@@ -35,6 +45,52 @@ export default function NavUser() {
         };
         getData();
     }, []);
+
+    useEffect(() => {
+        if (!currentUser) return;
+        // get a specific item
+        async function getUserInfo() {
+            const {data: userInfo, errors} = await client.models.UserInfo.list({
+                filter: {
+                    owner: {
+                        contains: currentUser.userId,
+                    },
+                },
+            });
+            userInfo.forEach((info) => {
+                console.log(info.owner);
+                console.log(info.owner == currentUser.userId);
+            });
+            console.dir({data: userInfo, errors});
+            setCurrentUserInfo(userInfo[0]);
+        }
+        console.log(currentUser.userId);
+        getUserInfo();
+    }, [currentUser]);
+
+    useEffect(() => {
+        if (!currentUserInfo) return;
+        const profilePhotoPieces = currentUserInfo.profilePhoto.split("/");
+        if (profilePhotoPieces[0].includes("http")) {
+            setAvatarURL(
+                `https://api.toucanny.net/avatar?userid=${
+                    currentUser?.userId
+                }&w=${256}`
+            );
+        } else {
+            async function generateURL() {
+                const generatedURL = await getUrl({
+                    path: currentUserInfo.profilePhoto,
+                    options: {
+                        expiresIn: 86400,
+                    },
+                });
+                console.log(generatedURL.url.toString());
+                setAvatarURL(generatedURL.url.toString());
+            }
+            generateURL();
+        }
+    }, [currentUser?.userId, currentUserInfo]);
 
     const router = useRouter();
     const handleSignOut = async () => {
@@ -48,45 +104,14 @@ export default function NavUser() {
             <SidebarMenuItem>
                 <DropdownMenu>
                     <DropdownMenuTrigger asChild>
-                        <SidebarMenuButton
-                            size="lg"
-                            className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-                        >
-                            <Avatar className="h-8 w-8 rounded-full">
-                                <AvatarImage
-                                    src={`https://api.toucanny.net/avatar?userid=${
-                                        currentUser?.userId
-                                    }&w=${256}`}
-                                    alt={currentUser?.userId}
-                                />
-                                <AvatarFallback className="rounded-full">
-                                    CN
-                                </AvatarFallback>
-                            </Avatar>
-                            <div className="grid flex-1 text-left text-sm leading-tight">
-                                <span className="truncate font-semibold">
-                                    {currentUser?.userId}
-                                </span>
-                                <span className="truncate text-xs">
-                                    {currentUser?.signInDetails?.loginId}
-                                </span>
-                            </div>
-                            <ChevronsUpDown className="ml-auto size-4" />
-                        </SidebarMenuButton>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent
-                        className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
-                        side={isMobile ? "bottom" : "right"}
-                        align="end"
-                        sideOffset={4}
-                    >
-                        <DropdownMenuLabel className="p-0 font-normal">
-                            <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                        {currentUserInfo && (
+                            <SidebarMenuButton
+                                size="lg"
+                                className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                            >
                                 <Avatar className="h-8 w-8 rounded-full">
                                     <AvatarImage
-                                        src={`https://api.toucanny.net/avatar?userid=${
-                                            currentUser?.userId
-                                        }&w=${256}`}
+                                        src={avatarURL}
                                         alt={currentUser?.userId}
                                     />
                                     <AvatarFallback className="rounded-full">
@@ -95,13 +120,46 @@ export default function NavUser() {
                                 </Avatar>
                                 <div className="grid flex-1 text-left text-sm leading-tight">
                                     <span className="truncate font-semibold">
-                                        {currentUser?.userId}
+                                        {currentUserInfo.firstName}{" "}
+                                        {currentUserInfo.lastName}
                                     </span>
                                     <span className="truncate text-xs">
-                                        {currentUser?.signInDetails?.loginId}
+                                        @{currentUserInfo.username}
                                     </span>
                                 </div>
-                            </div>
+                                <ChevronsUpDown className="ml-auto size-4" />
+                            </SidebarMenuButton>
+                        )}
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent
+                        className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+                        side={isMobile ? "bottom" : "right"}
+                        align="end"
+                        sideOffset={4}
+                    >
+                        <DropdownMenuLabel className="p-0 font-normal">
+                            {currentUserInfo && (
+                                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                                    <Avatar className="h-8 w-8 rounded-full">
+                                        <AvatarImage
+                                            src={avatarURL}
+                                            alt={currentUser?.userId}
+                                        />
+                                        <AvatarFallback className="rounded-full">
+                                            CN
+                                        </AvatarFallback>
+                                    </Avatar>
+                                    <div className="grid flex-1 text-left text-sm leading-tight">
+                                        <span className="truncate font-semibold">
+                                            {currentUserInfo.firstName}{" "}
+                                            {currentUserInfo.lastName}
+                                        </span>
+                                        <span className="truncate text-xs">
+                                            @{currentUserInfo.username}
+                                        </span>
+                                    </div>
+                                </div>
+                            )}
                         </DropdownMenuLabel>
                         <DropdownMenuSeparator />
                         <DropdownMenuGroup>
