d8ee30a970975f3b3120320d216cc680cd84dfbe
Jeshwin Prince <jeshwinjprince@gmail.com>
2025-01-22 17:59:11 -0800

    <progress> Select a template from the project creation form

--- src/components/dashboard/createproject.tsx
+++ src/components/dashboard/createproject.tsx
@@ -2,7 +2,7 @@
 
 import {useEffect, useState} from "react";
 
-import {Check, ChevronsUpDown, Plus} from "lucide-react";
+import {Check, ChevronsUpDown, Heart, Plus} from "lucide-react";
 
 import {
     Dialog,
@@ -24,15 +24,19 @@ import {
     CommandItem,
     CommandList,
 } from "@/components/ui/command";
-import {cn} from "@/lib/utils";
+import {cn, formatNumber} from "@/lib/utils";
 import axios from "axios";
 import {SidebarMenuButton} from "../ui/sidebar";
+import Image from "next/image";
+import {Badge} from "../ui/badge";
 
 const templates = [
     {
         title: "C",
+        value: "237e99ce-9534-4004-aaba-007ac4f40ac6",
         icon: "https://img.icons8.com/?size=100&id=Fn8H17bDpgYI&format=png&color=000000",
         author: "CodeNest",
+        description: "It's Simple, C?",
         verified: true,
         tags: ["Languages"],
         likes: 400,
@@ -40,8 +44,10 @@ const templates = [
     },
     {
         title: "C++",
+        value: "8ff664d7-d6b4-4a99-a4ac-0629fa3136f7",
         icon: "https://img.icons8.com/?size=100&id=SQcfmIiaxhkx&format=png&color=000000",
         author: "CodeNest",
+        description: "Need to pass your class? Get a C++!",
         verified: true,
         tags: ["Languages"],
         likes: 4,
@@ -49,8 +55,10 @@ const templates = [
     },
     {
         title: "Java",
+        value: "df562a12-9cff-4fb3-8de3-cb2f7bf59cb7",
         icon: "https://img.icons8.com/?size=100&id=h91o1gYEM6Ac&format=png&color=000000",
         author: "CodeNest",
+        description: "Programmers love coffee for a reason.",
         verified: false,
         tags: ["Languages"],
         likes: 4000,
@@ -58,8 +66,10 @@ const templates = [
     },
     {
         title: "Node.js",
+        value: "2c9625b0-04af-471b-b0a8-0c76c8f6a598",
         icon: "https://img.icons8.com/?size=100&id=phQ9SN4F3icL&format=png&color=000000",
         author: "Robert Cubeshorts",
+        description: "Who lives in a pineapple under the sea?",
         verified: true,
         tags: ["Languages", "Web"],
         likes: 40000,
@@ -67,7 +77,10 @@ const templates = [
     },
     {
         title: "Flutter",
+        value: "d64d0a13-aa41-481f-83b9-b9add0dd6e99",
         icon: "https://img.icons8.com/?size=100&id=asj7t1w9cjIC&format=png&color=000000",
+        description:
+            "I don't have any jokes for this. I genuinely like Flutter!",
         author: "Google",
         verified: false,
         tags: ["Frameworks", "Mobile"],
@@ -75,6 +88,76 @@ const templates = [
         uses: 9,
     },
 ];
+
+function TemplateOption({
+    template,
+}: {
+    template: {
+        title: string;
+        value: string;
+        icon: string;
+        author: string;
+        description: string;
+        verified: boolean;
+        tags: string[];
+        likes: number;
+        uses: number;
+    };
+}) {
+    return (
+        <div className="flex space-x-4 w-full">
+            <Image
+                src={template.icon}
+                alt={template.title}
+                width={80}
+                height={80}
+                className="size-20"
+            />
+            <div className="flex flex-col gap-2 justify-between w-full h-16">
+                <div className="flex items-center gap-1">
+                    <div className="text-lg whitespace-nowrap">
+                        {template.title}
+                    </div>
+                    {template.tags.map((tag, index) => (
+                        <div
+                            className="flex items-center h-fit py-1 px-2 bg-primary rounded-full text-foreground"
+                            style={{
+                                fontSize: "0.75rem",
+                                lineHeight: "1rem",
+                            }}
+                            key={index}
+                        >
+                            {tag}
+                        </div>
+                    ))}
+                </div>
+                <div className="text-xs">{template.description}</div>
+                <div className="flex justify-between">
+                    <div className="text-xs whitespace-nowrap flex gap-1">
+                        <Image
+                            src={`https://api.toucanny.net/avatar?userid=${template.author}?width=16`}
+                            alt={template.author}
+                            width={16}
+                            height={16}
+                            className="size-4 rounded-full"
+                        />
+                        @{template.author}
+                    </div>
+                    <div className="flex gap-2">
+                        <div className="flex items-center gap-0.5">
+                            <Heart />
+                            {formatNumber(template.likes)}
+                        </div>
+                        <div className="flex items-center gap-0.5">
+                            <Plus />
+                            {formatNumber(template.uses)}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
 
 export default function CreateProjectButton({
     sidebar,
@@ -126,7 +209,7 @@ export default function CreateProjectButton({
                     </Button>
                 )}
             </DialogTrigger>
-            <DialogContent className="w-fit max-w-2xl">
+            <DialogContent className="w-[500px] max-w-2xl">
                 <DialogHeader>
                     <DialogTitle>Create Project</DialogTitle>
                     <DialogDescription>
@@ -138,6 +221,7 @@ export default function CreateProjectButton({
                     <Input id="project-name" defaultValue={defaultName}></Input>
                     <Label>Template</Label>
                     <Popover
+                        modal
                         open={openTemplateSelect}
                         onOpenChange={setOpenTemplateSelect}
                     >
@@ -151,25 +235,25 @@ export default function CreateProjectButton({
                                 {selectedTemplate
                                     ? templates.find(
                                           (framework) =>
-                                              framework.title ===
+                                              framework.value ===
                                               selectedTemplate
                                       )?.title
                                     : "Select framework..."}
                                 <ChevronsUpDown className="opacity-50" />
                             </Button>
                         </PopoverTrigger>
-                        <PopoverContent className="p-0">
+                        <PopoverContent className="w-[480px] p-0">
                             <Command>
-                                <CommandInput placeholder="Search framework..." />
+                                <CommandInput placeholder="Search templates..." />
                                 <CommandList>
                                     <CommandEmpty>
-                                        No framework found.
+                                        No matching templates.
                                     </CommandEmpty>
                                     <CommandGroup>
                                         {templates.map((template) => (
                                             <CommandItem
-                                                key={template.title}
-                                                value={template.title}
+                                                key={template.value}
+                                                value={template.value}
                                                 onSelect={(currentValue) => {
                                                     setSelectedTemplate(
                                                         currentValue ===
@@ -181,16 +265,15 @@ export default function CreateProjectButton({
                                                         false
                                                     );
                                                 }}
+                                                className={` ${
+                                                    selectedTemplate ===
+                                                    template.value
+                                                        ? "border border-primary"
+                                                        : ""
+                                                }`}
                                             >
-                                                {template.title}
-                                                <Check
-                                                    className={cn(
-                                                        "ml-auto",
-                                                        selectedTemplate ===
-                                                            template.title
-                                                            ? "opacity-100"
-                                                            : "opacity-0"
-                                                    )}
+                                                <TemplateOption
+                                                    template={template}
                                                 />
                                             </CommandItem>
                                         ))}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,25 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import {clsx, type ClassValue} from "clsx";
+import {twMerge} from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
+}
+
+// Helper function to format count numbers into readable strings
+export function formatNumber(num: number) {
+    if (!num) return 0;
+    if (num < 1000) {
+        return num.toString(); // Display full number for anything below 1000
+    }
+
+    const units = ["k", "M", "B", "T"];
+    let unitIndex = -1;
+
+    while (num >= 1000 && unitIndex < units.length - 1) {
+        num /= 1000;
+        unitIndex++;
+    }
+
+    // Round to two significant figures for anything >= 1000
+    return `${num.toPrecision(3)}${units[unitIndex]}`;
 }
